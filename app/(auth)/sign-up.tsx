7209231b8d854da3d69aedcf1ef5a009635f6ed7
José Langa <joselanga02@gmail.com>
2024-09-01 11:25:24 +0530

    creating components and views

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,10 +1,41 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, ScrollView, View, Image } from "react-native";
+import { icons, images } from "@/constants";
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+
 const SignUp = () => {
   return (
-    <SafeAreaView>
-      <Text>SignUp </Text>
-    </SafeAreaView>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Create Your Account
+          </Text>
+        </View>
+        <View className="p-5">
+          <InputField
+            label="Name"
+            placeholder="Enter name"
+            icon={icons.person}
+          />
+          <InputField
+            label="Email"
+            placeholder="Enter email"
+            icon={icons.email}
+            textContentType="emailAddress"
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter password"
+            icon={icons.lock}
+            secureTextEntry={true}
+            textContentType="password"
+          />
+          <CustomButton title="Sign Up" className="mt-6" />
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 export default SignUp;
